da41f70b7bd644e0821672db1cdb59098b28a700
Cristobal Heiss <barbacoabq@gmail.com>
2021-02-07 20:35:08 +0100

    refactor functions and reset method

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ const StopWatch = function () {
   let isStopped = true;
   let internalDuration = 0;
 
-  const computeTime = function () {
+  const computeTime = () => {
     const latestValue = Date.now();
     const timePassed = (latestValue - initialValue) / 1000;
     return Math.round(timePassed * 100) / 100;
   };
 
   // Public
-  this.start = function () {
+  this.start = () => {
     if (isStopped) {
       isStopped = false;
       initialValue = Date.now();
@@ -20,7 +20,7 @@ const StopWatch = function () {
     }
   };
 
-  this.stop = function () {
+  this.stop = () => {
     if (!isStopped) {
       isStopped = true;
       internalDuration = internalDuration + computeTime();
@@ -29,15 +29,14 @@ const StopWatch = function () {
     }
   };
 
-  this.reset = function () {
+  this.reset = () => {
     internalDuration = 0;
     initialValue = 0;
+    isStopped = true;
   };
 
   Object.defineProperty(this, 'duration', {
-    get: function () {
-      return internalDuration;
-    },
+    get: () => internalDuration,
   });
 };
 
